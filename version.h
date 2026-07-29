9893d34b7e093dcf7f73af6665d61d693a610976
pdx15 <spdx15@gmail.com>
2026-07-29 12:05:17 +0500

    cosmetic

---
+++ version.h
@@ -0,0 +1,15 @@
+#pragma once
+
+#define APP_VER_MAJOR 1
+#define APP_VER_MINOR 0
+#define APP_VER_PATCH 2
+#define APP_VER_BUILD 0
+
+#define APP_VERSION_STR "1.0.3.0"
+
+#define APP_COMPANY "Yarche"
+#define APP_FILE_DESC "Diablo 4 Rotation Spam"
+#define APP_PRODUCT_NAME "Diablo 4 Overlay Tool"
+#define APP_COPYRIGHT "Copyright (C) 2026"
+#define APP_INTERNAL_NAME "d4rt.exe"
+#define APP_ORIGINAL_FILENAME "d4rt.exe"
